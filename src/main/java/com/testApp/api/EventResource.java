321481c5280f50e548cf520cc0e1e7a8ff7650da
wasadmin <wasadmin@DESKTOP-EEVPRA1.localdomain>
2020-04-21 16:13:34 -0400

    Add the List<Event>

--- src/main/java/com/testApp/api/EventResource.java
+++ src/main/java/com/testApp/api/EventResource.java
@@ -1,6 +1,7 @@
 package com.testApp.api;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class EventResource {
 	}
 	
 	@GetMapping("/get-all-events")
-	public Event getAllEvents() {
+	public List<Event> getAllEvents() {
 		//TODO
 		return null;
 	}
