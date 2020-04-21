6933b28efbd77262974a75c3e783848e6bc18cb2
wasadmin <wasadmin@DESKTOP-EEVPRA1.localdomain>
2020-04-21 15:50:41 -0400

    Add new main class EventSystemAPI

---
+++ src/main/java/com/testApp/EventSystemApi.java
@@ -0,0 +1,13 @@
+package com.testApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSystemApi {
+
+	public static void main (String[] args) {
+		SpringApplication.run(EventSystemApi.class, args);
+	}
+	
+}
