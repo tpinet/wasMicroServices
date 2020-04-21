976bad19b9b74d261d9068b6bcc55cc8919430e6
Karen <wasadmin@DESKTOP-EEVPRA1.localdomain>
2020-04-21 16:13:34 -0400

    Add the List<Event>

--- src/main/java/com/testApp/api/EventResource.java
+++ src/main/java/com/testApp/api/EventResource.java
@@ -1,6 +1,7 @@
 package com.testApp.api;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,12 @@ public class EventResource {
 
 	@GetMapping("/get-test-event")
 	public Event getTestEvent() {
-		return new Event(0, "Wine Tasting", new Date(), "For wine tasters and chuggers alike, come sample our best swill", 
+		return new Event(0, "Rum Tasting", new Date(), "For wine tasters and chuggers alike, come sample our best swill", 
 				"My front porch", 50);
 	}
 	
 	@GetMapping("/get-all-events")
-	public Event getAllEvents() {
+	public List<Event> getAllEvents() {
 		//TODO
 		return null;
 	}
