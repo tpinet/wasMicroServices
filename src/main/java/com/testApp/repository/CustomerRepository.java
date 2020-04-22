1c5dbc37a968e003482e28f06e4b6ec02f3879d0
wasadmin <wasadmin@DESKTOP-EEVPRA1.localdomain>
2020-04-22 16:21:14 -0400

    Connected the Customer to the JPA repository

--- src/main/java/com/testApp/repository/CustomerRepository.java
+++ src/main/java/com/testApp/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.testApp.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.testApp.api.Customer;
+
 public interface CustomerRepository extends CrudRepository<Customer, Integer>{
 
 }
