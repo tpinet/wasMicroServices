a6dcc14308d4eec1bc54a029f3aef856042c864d
sarahness <31962365+sarahness@users.noreply.github.com>
2020-04-22 16:39:32 -0400

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,15 @@
+
+
+-- ----------------------------------------------
+-- DDL Statements for tables
+-- ----------------------------------------------
+
+CREATE TABLE "EVENTS" ("ID" INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "NAME" VARCHAR(255), "DATE" TIMESTAMP,"DESCRIPTION" VARCHAR(255), "LOCATION" VARCHAR(255), "REGISTRATIONLIMIT" VARCHAR(255) );
+CREATE TABLE "CUSTOMERS" ("ID" INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), "NAME" VARCHAR(255), EMAIL VARCHAR(255));
+-- ----------------------------------------------
+-- DDL Statements for keys
+-- ----------------------------------------------
+
+-- primary/unique
+ALTER TABLE "EVENTS" ADD CONSTRAINT "SQL120325130144010" PRIMARY KEY ("ID");
+ALTER TABLE "CUSTOMERS" ADD CONSTRAINT "SQL120325130144011" PRIMARY KEY ("ID");
