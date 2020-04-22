2d9c0bfbc88d47d0aad6108e99c5e959895c063d
sarahness <31962365+sarahness@users.noreply.github.com>
2020-04-22 16:54:01 -0400

    Add files via upload

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,10 @@ insert into CUSTOMERS (NAME, EMAIL) VALUES ('Bruce', 'bruce@a.com');
 insert into CUSTOMERS (NAME, EMAIL) VALUES ('Paul', 'paul@b.com');
 insert into CUSTOMERS (NAME, EMAIL) VALUES ('Rick', 'rick@c.com');
 
-insert into EVENTS (ID, NAME, DATE, DESCRIPTION, LOCATION, REGISTRATIONLIMIT)
-values('99','Art Show','2010-05-12 00:00:00.0','Modern Art','Main Gallery',50);
-insert into EVENTS (ID, NAME, DATE, DESCRIPTION, LOCATION, REGISTRATIONLIMIT)
-values('100','Wine Tasting','2010-05-15 00:00:00.0','Pinot Affair','Main Gallery',50);
-insert into EVENTS (ID, NAME, DATE, DESCRIPTION, LOCATION, REGISTRATIONLIMIT)
-values('101','Ballet','2010-05-20 00:00:00.0','Swan Lake','Theatrey',300);
+insert into EVENTS (NAME, DATE, DESCRIPTION, LOCATION, REGISTRATIONLIMIT)
+values('Art Show','2010-05-12 00:00:00.0','Modern Art','Main Gallery',50);
+insert into EVENTS (NAME, DATE, DESCRIPTION, LOCATION, REGISTRATIONLIMIT)
+values('Wine Tasting','2010-05-15 00:00:00.0','Pinot Affair','Main Gallery',50);
+insert into EVENTS (NAME, DATE, DESCRIPTION, LOCATION, REGISTRATIONLIMIT)
+values('Ballet','2010-05-20 00:00:00.0','Swan Lake','Theatrey',300);
  
